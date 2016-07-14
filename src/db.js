c9bec8a0296cba058e1826e4f1de3b8a61462ec4
Melissa Marshall <mveritym@gmail.com>
2016-07-14 09:11:29 +0100

    Connecting schema to firebase db properly

---
+++ src/db.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyB3E0ke8eVwu67tIiF7Iab2V_yW0vDXmZ0",
+  authDomain: "strongcurves-43799.firebaseapp.com",
+  databaseURL: "https://strongcurves-43799.firebaseio.com",
+  storageBucket: "strongcurves-43799.appspot.com"
+};
+
+firebase.initializeApp(config);
+const database = firebase.database();
+
+export default {
+  getWorkouts: () => database.ref('workouts').once('value', snapshot => snapshot.val())
+};
--- src/schema.js
+++ src/schema.js
@@ -2,8 +2,44 @@ import {
   graphql,
   GraphQLSchema,
   GraphQLObjectType,
-  GraphQLString
+  GraphQLString,
+  GraphQLList,
+  GraphQLNonNull
 } from 'graphql';
+import db from './db';
+
+const WorkoutType = new GraphQLObjectType({
+  name: 'Workout',
+  description: 'A workout',
+  fields: () => ({
+    id: {
+      type: GraphQLString,
+      description: 'unique id of the workout'
+    },
+    exercises: {
+      type: new GraphQLList(GraphQLString),
+      description: 'list of ids of the exercises in the workout'
+    },
+    letter: {
+      type: GraphQLString,
+      description: 'letter of the workout (A, B or C)'
+    },
+    stage: {
+      type: GraphQLString,
+      description: 'stage number of the workout (1, 2 or 3)'
+    }
+  })
+});
+
+const generateUniqId = () => {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+};
 
 const LogBookSchema = new GraphQLSchema({
   query: new GraphQLObjectType({
@@ -13,6 +49,18 @@ const LogBookSchema = new GraphQLSchema({
         type: GraphQLString,
         resolve() {
           return 'world';
+        }
+      },
+      workouts: {
+        type: new GraphQLList(WorkoutType),
+        resolve() {
+          return db.getWorkouts().then(snapshot => {
+            const workouts = snapshot.val().slice(1);
+            return workouts.map(workout => ({
+              ...workout,
+              id: generateUniqId()
+            }));
+          });
         }
       }
     }
