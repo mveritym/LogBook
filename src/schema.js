962abe60cdd211463f0be86a882ee81510999be3
Melissa Marshall <mveritym@gmail.com>
2016-07-16 15:25:48 +0100

    Extracting number generator, cleaning up a bit

--- src/schema.js
+++ src/schema.js
@@ -1,43 +1,22 @@
 import {
-  graphql,
   GraphQLSchema,
   GraphQLObjectType,
-  GraphQLString,
   GraphQLList
 } from 'graphql';
-
-import { WorkoutType } from './types';
 import db from './db';
 
-const generateUniqId = () => {
-  function s4() {
-    return Math.floor((1 + Math.random()) * 0x10000)
-      .toString(16)
-      .substring(1);
-  }
-  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-    s4() + '-' + s4() + s4() + s4();
-};
+import { WorkoutType } from './types';
+import generateUniqId from './utils/unique-generator';
 
 const LogBookSchema = new GraphQLSchema({
   query: new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
-      hello: {
-        type: GraphQLString,
-        resolve() {
-          return 'world';
-        }
-      },
       workouts: {
         type: new GraphQLList(WorkoutType),
         resolve() {
           return db.getWorkouts().then(snapshot => {
-            const workouts = snapshot.val().slice(1);
-            return workouts.map(workout => ({
-              ...workout,
-              id: generateUniqId()
-            }));
+            return snapshot.val().slice(1);
           });
         }
       }
