1319fc35289e0fad348636b2490fd907c7dd129f
Melissa Marshall <mveritym@gmail.com>
2016-07-16 15:31:16 +0100

    Extracting query from schema

---
+++ src/query.js
@@ -0,0 +1,23 @@
+import {
+  GraphQLObjectType,
+  GraphQLList
+} from 'graphql';
+
+import db from './db';
+import { WorkoutType } from './types';
+
+const query = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    workouts: {
+      type: new GraphQLList(WorkoutType),
+      resolve() {
+        return db.getWorkouts().then(snapshot => {
+          return snapshot.val().slice(1);
+        });
+      }
+    }
+  }
+});
+
+export default query;
--- src/schema.js
+++ src/schema.js
@@ -1,27 +1,8 @@
-import {
-  GraphQLSchema,
-  GraphQLObjectType,
-  GraphQLList
-} from 'graphql';
-import db from './db';
-
-import { WorkoutType } from './types';
-import generateUniqId from './utils/unique-generator';
+import { GraphQLSchema } from 'graphql';
+import query from './query';
 
 const LogBookSchema = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: {
-      workouts: {
-        type: new GraphQLList(WorkoutType),
-        resolve() {
-          return db.getWorkouts().then(snapshot => {
-            return snapshot.val().slice(1);
-          });
-        }
-      }
-    }
-  })
+  query
 });
 
 export default LogBookSchema;
